bd820db9597b9b1b3de3fba84f535f4d5c08de68
dobrayAnika <anyaadobrova@gmail.com>
2023-12-28 18:39:20 +0300

    появилось окошко с ошибками

--- src/main/java/com/cgvsu/GuiController.java
+++ src/main/java/com/cgvsu/GuiController.java
@@ -350,22 +350,39 @@ public class GuiController {
             mesh = ObjReader.read(fileContent);
             TriangulatedModelWithCorrectNormal triangulatedModelWithCorrectNormal = new TriangulatedModelWithCorrectNormal(mesh);
             transformedModel = new TransformedModel(triangulatedModelWithCorrectNormal, new AffineTransf());
-            //Model model = ObjReader.read(fileContent);
             transformedModel.getTriangulatedModel().getInitialModel().modelName = objName;
             models.add(transformedModel.getTriangulatedModel().getInitialModel());
 
-
-            //models.add(model);
-            //models.add(transformedModel.getTriangulatedModel().getInitialModel());
-
-
-
-            // todo: обработка ошибок
             updateModelComboBox();
         } catch (IOException exception) {
-
+            // Ошибка при чтении файла. Вывести окно с сообщением об ошибке.
+            showErrorAlert("Error Reading File", "An error occurred while reading the file.");
+        } catch (ObjReaderException objReaderException) {
+            // Ошибка разбора файла OBJ. Вывести окно с сообщением об ошибке.
+            showErrorAlert("Error Parsing OBJ File", objReaderException.getMessage());
+        } catch (IncorrectFileException incorrectFileException) {
+            // Ошибка формата файла. Вывести окно с сообщением об ошибке.
+            showErrorAlert("Incorrect File Format", incorrectFileException.getMessage());
         }
     }
+
+    private void showErrorAlert(String title, String content) {
+        Alert alert = new Alert(Alert.AlertType.ERROR);
+        alert.setTitle(title);
+        alert.setHeaderText(null);
+        alert.setContentText(content);
+
+        ButtonType okButton = new ButtonType("Ok", ButtonBar.ButtonData.OK_DONE);
+        alert.getButtonTypes().setAll(okButton);
+
+        // Обработчик события для кнопки "Ок"
+        alert.showAndWait().ifPresent(response -> {
+            if (response == okButton) {
+                // Действия после нажатия кнопки "Ок", если необходимо
+            }
+        });
+    }
+
 
     @FXML
     private void onSaveModelMenuItemClick() {
